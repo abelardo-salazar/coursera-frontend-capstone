da93bd9249fc4a3e98f3cf9a7e7194328794681a
Abelardo Salazar <abelardosalazar94@gmail.com>
2023-11-02 21:02:22 -0400

    HTML base semantic structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React from 'react';
 import './App.css';
+import { Header } from './components/layout/Header';
+import { Nav } from './components/layout/Nav';
+import { Main } from './components/layout/Main';
+import { Footer } from './components/layout/Footer';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <React.Fragment className="App">
+        <Header>
+        </Header>
+          <Nav></Nav>
+        <Main></Main>
+        <Footer></Footer>
+    </React.Fragment>
   );
 }
+
 
 export default App;
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const logo = "/images/logo.png"
+
+export const Header = () => {
+  return (
+    <header>
+        <img src={logo} alt="Little lemon logo"  />
+    </header>
+  )
+}
